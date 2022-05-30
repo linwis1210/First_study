75c34b8d4f46751fe467d6fb23c5cce395e16859
linwis1210 <277533694@qq.com>
2022-05-30 20:43:30 +0800

    add util

---
+++ util/cutImg.py
@@ -0,0 +1,44 @@
+import os
+import shutil
+from PIL import Image
+import numpy as np
+from PIL import ImageFile
+ImageFile.LOAD_TRUNCATED_IMAGES = True
+Image.MAX_IMAGE_PIXELS = None
+
+"""
+对图片进行切割
+"""
+
+img_dir = r"F:\HSI\seg\JPEGImages"
+img_list = os.listdir(img_dir)
+
+num = 1
+for img_name in img_list:
+    img = Image.open(os.path.join(img_dir, img_name)).convert("RGB")
+    [src_h, src_w] = img.size
+    [src_h, src_w] = src_h - src_h % 1000, src_w - src_w % 1000
+    img = img.resize((src_h, src_w), Image.BILINEAR)
+
+    img = np.asarray(img)
+    cut_size = [1000, 1000]
+    [row, col, _] = img.shape
+
+    h = row // cut_size[0]
+    w = col // cut_size[1]
+
+    img_path = "F:/HSI/seg/jpg2/" + str(num) + "_"
+    num = num+1
+    img_name = 0
+    for i in range(h):
+        for j in range(w):
+            ROI = img[i*cut_size[0]:(i+1)*cut_size[0], j*cut_size[1] : (j+1)*cut_size[1], :]
+            ROI_img = Image.fromarray(ROI)
+            ROI_img.save(img_path + str(img_name)+".jpg")
+            img_name = img_name + 1
+
+
+
+
+
+
